4335d17c7f804788b1ed66764fede1c30ee34f43
vrct <furkanarican@gmail.com>
2018-01-29 01:05:41 +0300

    Github worker modified

--- github_worker_auth.py
+++ github_worker_auth.py
@@ -1,31 +1,45 @@
+import os
 import requests
-import json
+import redis
 
-token = 'b1141d703757e42807c5c6bfc314c99041342a5b'
-base_url = 'https://api.github.com/users/{}/starred?access_token={}&page={}'.format('{}',token,'{}')
+try:
+    github_token = os.getenv("GITHUB_TOKEN")
+except KeyError:
+    raise Exception("Please set environment variables GITHUB_TOKEN")
 
-def get_stars(username):
+github_subs = []
+github_api_url = 'https://api.github.com/users/{}/starred'
+github_params = {'access_token':github_token}
 
-    page = 1
-
-    flag = True
-    star_dict = {}
-
-    while flag :
-
-        url = base_url.format(username,page)
-        response = requests.get(url).json()
-
-        if len(response) == 0:
-            flag = False
-
-        else:
-
-            for single_star in response:
-                star_dict[single_star['id']] = [single_star['html_url'],single_star['description']]
+GITHUB_STAR_SET = "Github:Stars:{}"
+GITHUB_REPO_KEY = "Github:Repos:{}"
 
 
-        page += 1
+r = redis.Redis()
 
-    print (star_dict)
-#stardict is final
+def is_exists_in_db(user_star_set, repo_id):
+    return r.sismember(user_star_set, repo_id)
+
+def add_into_db(user_star_set, starred_repo):
+    _id = starred_repo.get('id')
+    r.sadd(user_star_set, _id)  #add repo_id's to subscription set
+    r.hmset(GITHUB_REPO_KEY.format(_id),{'name':starred_repo['name'],'url':starred_repo['html_url']})    #add repo_name and repo_url to repo_id's hash
+
+for sub in github_subs:
+    _url = github_api_url.format(sub)
+
+    github_params["page"] = 1
+
+    while True:
+        response = requests.get(url=_url,params=github_params).json()
+
+        if not response:
+            break
+
+        for repo in response:
+            if is_exists_in_db(GITHUB_STAR_SET.format(sub), repo):
+                break
+            else:
+                add_into_db(GITHUB_STAR_SET.format(sub), repo)
+
+            github_params["page"] += 1
