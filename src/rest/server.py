50341e800867cb09484e93b3d46c16aa0e9f5b15
Duygu Gündoğdu <c1411026@student.cankaya.edu.tr>
2018-01-28 11:21:08 +0300

    add rest api

--- src/rest/server.py
+++ src/rest/server.py
@@ -4,62 +4,51 @@ import json
 from graceful.serializers import BaseSerializer
 from graceful.fields import StringField, BaseField
 from graceful.resources.generic import RetrieveAPI, ListCreateAPI
-from src.rest.rpc_client import RpcClient
+#from src.rest.rpc_client import RpcClient
 
 
 api = application = falcon.API()
 
 
-class ListOfStringsField(BaseField):
+class JsonField(BaseField):
     """
     Represents the list of strings type field in our api.
     """
     def from_representation(self, data):
-        return data
+        return json.dumps(data)
 
     def to_representation(self, value):
-        return value
+        return json.loads(value)
 
 
-# this is how we represent cats in our API
 class SubscriptionSerializer(BaseSerializer):
-    # todo add validators to the fields.
+    #TODO: ADD VALIDATORS
     email = StringField("Email to send repository reports", allow_null=False)
-    subscriptions = ListOfStringsField("List of subscribed github ids",
+    subscriptions = JsonField("List of subscribed github ids",
                                        allow_null=False)
     period = StringField("Notification period e.g. daily, or weekly",
                          allow_null=False)
-
-
-class Subscription(RetrieveAPI, with_context=True):
-    """
-    """
-    def __init__(self, rpc_client):
-        self.rpc_client = rpc_client
-        super(Subscription, self).__init__()
-    serializer = SubscriptionSerializer()
-
-    def retrieve(self, params, meta, **kwargs):
-        pass
+    telegramID = StringField("Telegram ID", allow_null = False)
 
 
 class SubscriptionList(ListCreateAPI, with_context=True):
     """
     """
-    def __init__(self, rpc_client):
-        self.rpc_client = rpc_client
+    def __init__(self):
+        #self.rpc_client = rpc_client
         super(SubscriptionList, self).__init__()
 
     serializer = SubscriptionSerializer()
 
     def create(self, params, meta, **kwargs):
         validated = kwargs.get('validated')
+        print(validated)
+        return validated
         # todo make the rpc call and wait for response
-        resp = self.rpc_client.rpc_call("subscribe", validated)
-        print(resp)
-        return resp
+        #resp = self.rpc_client.rpc_call("subscribe", validated)
+        #print(resp)
+        #return resp
 
 
-rpc_client = RpcClient()
-api.add_route("/v1/subscriptions/{email}", Subscription(rpc_client))
-api.add_route("/v1/subscriptions/", SubscriptionList(rpc_client))
+#rpc_client = RpcClient()
+api.add_route("/v1/subscriptions/", SubscriptionList())
