a4edf21f562a13d0949b8f3c346bef505cce4717
sipirsipirmin <tunahandursun@hotmail.com>
2018-01-28 11:23:26 +0300

    paralelleştirme denemesi

--- src/scheduler/deneme.py
+++ src/scheduler/deneme.py
@@ -1,14 +1,16 @@
-import schedule, time
+import threading,schedule, time
 
 def fikibok():
 	print("fikibok")
 
-def allektamovikmovik():
-	print("hello from func 2")
+def allektamovikmovik(): 
+	while 1:
+		schedule.run_pending()
+		time.sleep(1)
 
 schedule.every(0.1).minutes.do(fikibok)
-schedule.every().day.at("10:12").do(allektamovikmovik)
+threading.Thread(target=allektamovikmovik).start()
 
 while True:
-	schedule.run_pending()
-	time.sleep(1)
+	print("main: şimdilik boş boş takılıyorum")
+	time.sleep(2)
