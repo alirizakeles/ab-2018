436ca4630a434358de75138e65ebbf7987ecd337
sipirsipirmin <tunahandursun@hotmail.com>
2018-01-29 10:48:29 +0300

    redis bağlantıları yapıldı

--- src/scheduler/deneme.py
+++ src/scheduler/deneme.py
@@ -1,30 +1,42 @@
 import threading, schedule
 from time import sleep
+import redis
+import json
+
+redis_cli = redis.Redis()
+sample_data = """{
+	'email': 'tunahan@dursun.com',
+	'repoCount':2,
+	'period':'weekly',
+	'telegramId':'asdf',
+	'subscriptions':{
+		'github':['anilaydin'],
+		'gitlab':['aliriza']
+	}
+}"""
 # Bu kod parçacığı, rabitten okuduğu veriyi,  redise yazar ve workerlara iş bırakır.
-# notifier a veri bırakır
 
-def rabitten_oku():
-	redise_yazdirmasyon()
-	schedleri_ayarla()
-	tempi_sil()
-
-def schedleri_ayarla():
+def schedleri_ayarla(kullanici_verisi):
 	# redise veriler yazıldıktan sonra workerlardan starları çekecek olan workerlara rabbit üzerinden 
 	# veri bırakır. notifiera iş bırakmayacak.
+	period = kullanici_verisi['period']
 
-	# schedule.every(5).minutes.do(workira_is_birak)
-	sleep(1)
-	pass
+	if period == "daily":
+		schedule.every(1).days.at("10:10").do(workira_is_birak)
 
-def redise_yazdirmasyon():
+	else if period ==  "weekly":
+		schedule.every(7).days.at("10:10").do(workira_is_birak)
+
+	else if period == "monthly":
+		schedule.every(30).days.at("10:10").do(workira_is_birak)
+
+def redise_yazdir(kullanici_verisi):
 	# rabitten okunan veriler, redise kaydediliyor
-	sleep(1)
-	pass
+	redis_cli.set(':'.join(["User",kullanici_verisi['email']],json.dumps(kulanici_verisi))
 
-def tempi_sil():
+def tempi_sil(kullanici_verisi):
 	# rediste bulunan, kullanıcıya ait verilerin bulunduğu datayı redisten siliyor
-	sleep(1)
-	pass
+	redis_cli.delete(":".join(["temp","User",kullanici_verisi['email']])) 
 
 def notifiera_is_birak():
 	# Kullanıcılara eposta yollanması için gerekli iş notifieara bırakılacak
@@ -43,9 +55,13 @@ schedule.every(5).minutes.do(workira_is_birak)
 threading.Thread(target=allektamovikmovik).start()
 
 while True:
-	# Burası rabiti dinleyecek. Yeni bir kullanıcı eklendiğinde rabitten_oku fonksiyonu çalıştırılacak
-	print("main: şimdilik boş boş takılıyorum")
-	rabitten_oku()
+	# Burası rabiti dinleyecek. Yeni bir kullanıcı eklendiğinde ilgili fonksiyonlar çalıştırılacak 
+	# rabitten sample_data verisinin geldiği varsayılıyor
+	kullanici_verisi = json.loads(sample_data)
+
+	redise_yazdir(kullanici_verisi)
+	schedleri_ayarla(kullanici_verisi)
+	tempi_sil(kullanici_verisi)
 
 	time.sleep(2)
 
