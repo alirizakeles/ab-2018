fe59b610a986164b0d6571ec7c5718694578c0c4
vrct <furkanarican@gmail.com>
2018-01-30 15:51:02 +0300

    Schedule notify added

--- schedule_notify.py
+++ schedule_notify.py
@@ -1,30 +1,67 @@
 import redis
 import json
 import random
+import pika
 
 
 r = redis.Redis()
-github_subs = []
 
-keys = ["Github:Stars:{}".format(sub) for sub in github_subs]
-unioned= r.sunion(keys=keys)
+def on_request(ch, method, properties, body):
+    user = json.loads(body.decode())
+    user_suggestion_key = 'User:Suggestions:{}'.format(user['email'])
+    git_keys = ["Github:Stars:{}".format(sub) for sub in user['subscriptions']['github']]
+    git_keys.append("Gitlab:Stars:{}".format(sub) for sub in user['subscriptions']['gitlab'])
+    if check_empty_set(user_suggestion_key):
+        r.sunionstore(dest=user_suggestion_key, keys=git_keys)
 
-chosen_repos = random.sample(unioned,count)
-chosen_repo_list = []
+    prepare_queue_works(user, user_suggestion_key,user['repoCount'], git_keys)
 
-for repo in chosen_repos:
-    repo_key = "Github:Repos:{}".format(repo.decode())
-    repo_info = r.hgetall(repo_key)
-    decoded_repo_info = {k.decode(): v.decode() for k, v in repo_info.items()}
-    chosen_repo_list.append(decoded_repo_info)
 
-email_dict = {b'to':b'user.email',b'repos':chosen_repo_list}
-telegram_dict = {b'to':b'telegram_id',b'repos':chosen_repo_list}
-decoded_email_dict = {k.decode(): v.decode() if type(v) is bytes else v for k, v in email_dict.items()}
-decoded_telegram_dict = {k.decode(): v.decode() if type(v) is bytes else v for k, v in telegram_dict.items()}
+def check_empty_set(user_suggestion_key):
+    if len(r.smembers(user_suggestion_key)) == 0:
+        flag = True
+    else:
+        flag = False
 
-work_email = json.dumps(decoded_email_dict)
-work_telegram = json.dumps(decoded_telegram_dict)
+    return flag
 
-r.lpush('email_queue',work_email)
-r.lpush('telegram_queue',work_telegram)
+def prepare_queue_works(user, user_suggestion_key, count, git_keys):
+    chosen_repo_list = []
+    ready_repos_list = []
+
+    if len(r.smembers(user_suggestion_key)) != 0 :
+        while count > 0:
+            if check_empty_set(user_suggestion_key) :
+                r.sunionstore(dest=user_suggestion_key, keys=git_keys)
+            count -= 1
+            chosen_repo_list.append(r.spop(user_suggestion_key))
+        for repo in chosen_repo_list:
+            repo_key = "Github:Repos:{}".format(repo.decode())
+            repo_info = r.hgetall(repo_key)
+            decoded_repo_info = {k.decode(): v.decode() for k, v in repo_info.items()}
+            ready_repos_list.append(decoded_repo_info)
+
+
+        email_dict = {'to':'{}'.format(user['email']),'repos':ready_repos_list}
+        if user['telegramId'] != None :
+                telegram_dict = {'to':'{}'.format(user['telegramId']),'repos':ready_repos_list}
+                work_telegram = json.dumps(telegram_dict)
+                r.lpush('telegram_queue',work_telegram)
+
+        work_email = json.dumps(email_dict)
+        r.lpush('email_queue',work_email)
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='work',
+                         exchange_type='direct')
+
+channel.queue_declare(queue='schedule_notifier_queue')
+
+channel.queue_bind(exchange='work', queue='schedule_notifier_queue',routing_key='schedule_notifier')
+
+channel.basic_consume(on_request, queue='schedule_notifier_queue', no_ack=True)
+
+channel.start_consuming()
